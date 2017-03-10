ccf2f1982c1e9c9cf18221ad12e050e487c771c7
yueludanfeng <yueludanfeng@gmail.com>
2017-03-10 10:37:24 +0800

    add some files

---
+++ testphp/VerificationCode/testvalidatecode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/7
+ * Time: 9:53
+ */
+include_once ("ValidateCode.php");
+$validation = new ValidateCode();
+$validation->doimg();
+?>
+<p>hello</p>
